6f6030d06f066fcd56c1ea92dd03019f8280548d
Niels <niels.lohmann@gmail.com>
2014-01-21 22:46:43 +0100

    create files in _publish

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+rm -fr _publish
+
+FILES=`ls -1`
+
+mkdir -p _publish
+
+for FILE in $FILES; do
+    cp -r $FILE _publish
+done
+
+cd _publish
+
 rm -fr resources/bibtex resources/html resources/python resources/teaching resources/xml
 rm -fr *.sh
-
-echo "The directory is now safe to publish. Do not commit this stage!"
