daef8a6304d22413eb1c3f63cd9e01ed6912dbb8
Niels <niels.lohmann@gmail.com>
2014-01-21 22:53:24 +0100

    publish script

--- publish.sh
+++ publish.sh
@@ -14,3 +14,12 @@ cd _publish
 
 rm -fr resources/bibtex resources/html resources/python resources/teaching resources/xml
 rm -fr *.sh
+
+find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
+
+rm -fr /users/teo00/nl/public_html/*
+cp -r * /users/teo00/nl/public_html
+
+cd ..
+rm -fr _publish
