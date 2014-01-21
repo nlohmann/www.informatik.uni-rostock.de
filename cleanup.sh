920d37f6a98c9fd14082e6124b53290ba0c41ec7
Niels <niels.lohmann@gmail.com>
2014-01-21 22:39:55 +0100

    cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -fr resources/bibtex resources/html resources/python resources/teaching resources/xml
+rm -fr *.sh
+
+echo "The directory is now safe to publish. Do not commit this stage!"
